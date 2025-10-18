f79b1509bbe2fa91a5da2f883ca95eff5b3d5975
Oliver Turk <turko@cardiff.ac.uk>
2025-10-18 22:14:14 +0100

    .

--- round_1059_B.cpp
+++ round_1059_B.cpp
@@ -1,17 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool is_plaindrome(string s)
+bool is_palindrome(const string &s)
 {
     int l = 0;
-    int r = s.length();
+    int r = s.length() - 1;
 
     while (l < r)
     {
-        if (s[l] != s[r])
-        {
-            return false;
-        }
+        if (s[l] != s[r]) return false;
 
         l += 1;
         r -= 1;
@@ -25,28 +22,48 @@ int main()
     int cases;
     cin >> cases;
 
-    for (int i=0; i < cases; i++)
+    for (int i=0; i < cases; ++i)
     {
         int length;
         cin >> length;
 
         string s;
         cin >> s;
+
+        vector<int> removed_idxs;
         
-        if (is_plaindrome(s))
+        if (is_palindrome(s))
         {   
-            cout << s.size();
-
-            for (int j=0; j < s.length(); j++)
-            {
-                cout << j;
-            }
-
+            cout << 0 << "\n";
             continue;
         }
-        
-        
+            
+        int index = 0;
 
+        auto it = remove_if(s.begin(), s.end(), [&](char c) 
+        {
+            if (c == '0')
+            {
+                removed_idxs.push_back(index);
+                ++index;
+                return true;
+            }
+            ++index;
+            return false;
+        });
+
+        s.erase(it, s.end());
+
+        if (is_palindrome(s))
+        {
+            cout << removed_idxs.size() << '\n';
+            for (int idx : removed_idxs) cout << idx + 1 << " ";
+            cout << '\n';
+            continue;
+        }
+
+        cout << -1;
+        cout << '\n';
     }
 }
 
